c28916a32db6dfb8e450fdd093bba0a602bb38a4
DESKTOP-I2PNSR7\denise <denise1993.hsu@gmail.com>
2021-12-14 17:25:17 +0800

    add HTML and CSS-2

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,34 @@
 /*=============== SHOW MENU ===============*/
+const navMenu = document.getElementById('nav-menu'),
+      navToggle = document.getElementById('nav-toggle'),
+      navClose = document.getElementById('nav-close');
 
+/*=====MENU SHOW=====*/
+/* Validate if constant exists*/
+if(navToggle){
+  navToggle.addEventListener('click', () => {
+    navMenu.classList.add('show-menu');
+  });
+}
+/*=====MENU CLOSE=====*/
+/* Validate if constant exists*/
+if(navClose){
+  navClose.addEventListener('click', ()=>{
+    navMenu.classList.remove('show-menu');
+  });
+}
 
 /*=============== REMOVE MENU MOBILE ===============*/
+const navLink = Array.from(document.querySelectorAll('.nav__link'));
+
+function linkAction(){
+  navMenu.classList.remove('show-menu');
+}
+
+navLink.forEach(nav=> nav.addEventListener('click', linkAction));
+
+
+
 
 
 /*=============== CHANGE BACKGROUND HEADER ===============*/
